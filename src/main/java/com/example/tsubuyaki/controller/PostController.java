8ba134dbd69a5a75f490f99b192f218841e6b8ce
meiji26hhhh <s-nagahama@ndsc.co.jp>
2026-07-01 13:52:44 +0900

    Revert "feat(post): GET /posts で最新50件を新着順に返す"
    This reverts commit 9f52c5fb0ad1be5ed8ea67479dc0c5f59688069c.

--- src/main/java/com/example/tsubuyaki/controller/PostController.java
+++ src/main/java/com/example/tsubuyaki/controller/PostController.java
@@ -17,7 +17,7 @@ public class PostController {
 
     @GetMapping({ "/", "/posts" })
     public String list(Model model) {
-        model.addAttribute("posts", postService.latest50());
+        model.addAttribute("posts", postService.latest());
         return "posts/list";
     }
 
